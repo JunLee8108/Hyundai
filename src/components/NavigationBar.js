69ec63ee9204e87eff080370072c860dbff5be63
Jun Lee <jun@Juns-MBP.lan>
2023-06-03 22:16:53 -0500

    used media query for nav bar and home page

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,43 +1,65 @@
 import "./NavigationBar.css";
 import { Link } from "react-router-dom";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 function NavigationBar() {
-  return (
-    <div className="nav-container">
-      <nav>
-        <img className="hyundai-logo" src="img/hyundai.png" alt="logo"></img>
-        <li className="company-name">
-          <Link className="nav-item-company" to="/">
-            Hyundai
-          </Link>
-        </li>
+  const isMobile = useMediaQuery("(max-width: 600px)");
 
-        <div className="nav-menu">
-          <li>
-            <Link className="nav-item-company" to="/Vehicles">
-              Vehicles
+  // If it's mobile
+  if (isMobile) {
+    return (
+      <div className="nav-container">
+        <nav>
+          <img className="hyundai-logo" src="img/hyundai.png" alt="logo"></img>
+          <li className="company-name">
+            <Link className="nav-item-company" to="/">
+              Hyundai
             </Link>
           </li>
-          <li>
-            <Link className="nav-item-company" to="/ShoppingTools">
-              Shopping Tools
+        </nav>
+        <div className="clear-both"></div>
+      </div>
+    );
+  }
+  // Not mobile
+  else {
+    return (
+      <div className="nav-container">
+        <nav>
+          <img className="hyundai-logo" src="img/hyundai.png" alt="logo"></img>
+          <li className="company-name">
+            <Link className="nav-item-company" to="/">
+              Hyundai
             </Link>
           </li>
-          <li>
-            <Link className="nav-item-company" to="/Owners">
-              Owners
-            </Link>
-          </li>
-          <li>
-            <Link className="nav-item-company" to="/ContactUs">
-              Contact Us
-            </Link>
-          </li>
-        </div>
-      </nav>
-      <div className="clear-both"></div>
-    </div>
-  );
+
+          <div className="nav-menu">
+            <li>
+              <Link className="nav-item-company" to="/Vehicles">
+                Vehicles
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-item-company" to="/ShoppingTools">
+                Shopping Tools
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-item-company" to="/Owners">
+                Owners
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-item-company" to="/ContactUs">
+                Contact Us
+              </Link>
+            </li>
+          </div>
+        </nav>
+        <div className="clear-both"></div>
+      </div>
+    );
+  }
 }
 
 export default NavigationBar;
