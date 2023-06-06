600ddee6ccc81bf2b73ce67484a77cbf8700bcf8
Jun Lee <jun@Juns-MBP-2.lan>
2023-06-06 16:27:40 -0500

    quick fix for the tablet view

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,72 @@
 import "./Footer.css";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import ioniq from "../assets/hyundia-ioniq-model2.jpg";
 
 function Footer() {
-  return (
-    <div>
-      <div className="footer-container">
-        <div className="footer-item">
-          <h1>Vehicles</h1>
-          <p>SUVs</p>
-          <p>Sedans</p>
-          <p>Electrified</p>
+  const isMobile = useMediaQuery("(max-width: 600px)");
+
+  if (isMobile) {
+    return (
+        <div>
+          <div className="footer-container">
+            <div className="footer-item">
+              <h1>Vehicles</h1>
+              <div className="arrow-symbol">
+              ▼
+              </div>
+              <div className="clean-both"></div>
+            </div>
+            <div className="footer-item">
+              <h1>Shopping Tools</h1>
+              <div className="arrow-symbol">
+              ▼
+              </div>
+              <div className="clean-both"></div>
+            </div>
+            <div className="footer-item">
+              <h1>Owners</h1>
+              <div className="arrow-symbol">
+              ▼
+              </div>
+              <div className="clean-both"></div>
+            </div>
+            <div className="footer-item">
+              <h1>About</h1>
+              <div className="arrow-symbol">
+              ▼
+              </div>
+              <div className="clean-both"></div>
+            </div>
+          </div>
         </div>
-        <div className="footer-item">
-          <h1>Shopping Tools</h1>
-          <p>Shop Hyundai</p>
+      );
+  }
+  else  {
+    return (
+        <div>
+          <div className="footer-container">
+            <div className="footer-item">
+              <h1>Vehicles</h1>
+              <p>SUVs</p>
+              <p>Sedans</p>
+              <p>Electrified</p>
+            </div>
+            <div className="footer-item">
+              <h1>Shopping Tools</h1>
+              <p>Shop Hyundai</p>
+            </div>
+            <div className="footer-item">
+              <h1>Owners</h1>
+              <p>Login to MyHyundai</p>
+            </div>
+            <div className="footer-item">
+              <h1>About</h1>
+              <p>Our Company</p>
+            </div>
+          </div>
         </div>
-        <div className="footer-item">
-          <h1>Owners</h1>
-          <p>Login to MyHyundai</p>
-        </div>
-        <div className="footer-item">
-          <h1>About</h1>
-          <p>Our Company</p>
-        </div>
-      </div>
-    </div>
-  );
+      );
+  }
 }
 
 export default Footer;
